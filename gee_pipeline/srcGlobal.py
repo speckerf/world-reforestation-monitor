8d49e72a396b63c245632b8aac3c30eba5a55008
speckerf <felixspecker@hotmail.ch>
2024-09-16 16:52:09 +0200

    update sentinel-2 tile selection: don't use ecoregion-phenology anymore, instead use tile-based evi estimate. Use acquisition with highest evi scores throughout the year. For this assess the maximum evi score as the 95 percentile, and allow a max difference of 0.20 for every tile. In addition, some snow covered tiles experienced a high evi, so we need to filter out these pixels, before reducing the evi score. the problem remained: so we added a manual filtering based on the sentinel-2 metadata, where snow_ice_percentage is higher then vegetation_percentage, we exclude the tiles. Now we have smooth output images in the boreal, taiga regions, but also in the tropical regions, where the fixed phenology window was struggling in very sesasonaly precipitation variable regions

--- gee_pipeline/srcGlobal.py
+++ gee_pipeline/srcGlobal.py
@@ -8,6 +8,7 @@ import ee
 import numpy as np
 import pandas as pd
 from loguru import logger
+from tqdm import tqdm
 
 from config.config import get_config
 from gee_pipeline.utilsAngles import add_angles_from_metadata_to_bands
@@ -31,106 +32,70 @@ credentials = ee.ServiceAccountCredentials(
 ee.Initialize(credentials, project="ee-speckerfelix")
 
 
-def export_ecoregion_per_mgrs_tile(
-    eco_id: int | list[int],
-) -> None:
+def export_mgrs_tile(mgrs_tile: str) -> None:
+
     version = CONFIG_GEE_PIPELINE["PIPELINE_PARAMS"]["VERSION"]
     year = int(CONFIG_GEE_PIPELINE["PIPELINE_PARAMS"]["YEAR"])
     output_resolution = CONFIG_GEE_PIPELINE["PIPELINE_PARAMS"]["OUTPUT_RESOLUTION"]
 
-    logger.info(f"Exporting ecoregion: {eco_id}")
-    # this is used for initial filtering of the imagecollection in GEE, but not for export
-    ecoregions = ee.FeatureCollection("RESOLVE/ECOREGIONS/2017")
-
-    if isinstance(eco_id, int):
-        geometry = ecoregions.filter(ee.Filter.eq("ECO_ID", eco_id)).first().geometry()
-    elif isinstance(eco_id, list):
-        # merge geometries of multiple ecoregions
-        geometry = (
-            ecoregions.filter(ee.Filter.inList("ECO_ID", eco_id))
-            .union()
-            .first()
-            .geometry()
-        )
-    else:
-        raise ValueError("eco_id must be int or list[int]")
-
-    bounding_box_geometry = geometry.bounds()
-
-    # for export use rasterized ecoregion product instead.
-    resolve_ecoregions = ee.Image(
-        "projects/crowtherlab/Composite/CrowtherLab_Composite_30ArcSec"
-    ).select("Resolve_Ecoregion")
-    if isinstance(eco_id, int):
-        export_mask = resolve_ecoregions.eq(eco_id)
-    elif isinstance(eco_id, list):
-        export_mask = resolve_ecoregions.eq(eco_id[0])
-        for eco in eco_id[1:]:
-            export_mask = export_mask.Or(resolve_ecoregions.eq(eco))
-    else:
-        raise ValueError("eco_id must be int or list[int]")
-
+    logger.info(f"Exporting mgrs_tile: {mgrs_tile}")
     start_date = ee.Date(f"{year}-01-01")
     end_date = ee.Date(f"{year}-12-31")
 
+    # list all sentinel-2 tiles in this mgrs tile
+    all_mgrs_tiles = pd.read_csv(
+        os.path.join(
+            "data",
+            "gee_pipeline",
+            "outputs",
+            "mgrs_tiles",
+            "mgrs_tiles_all_land_ecoregions.csv",
+        )
+    )
+    current_mgrs_tiles = list(
+        set(
+            all_mgrs_tiles[all_mgrs_tiles["mgrs_tile_3"] == mgrs_tile][
+                "mgrs_tile"
+            ].tolist()
+        )
+    )
+
     # save s2_indices_filtered for later use
-    if isinstance(eco_id, int):
-        s2_indices_filename = f"s2-indices_{year}_eco-{eco_id}_{version}.txt"
-    elif isinstance(eco_id, list):
-        eco_ids_string = "-".join(map(str, eco_id))
-        s2_indices_filename = f"s2-indices_{year}_eco-{eco_ids_string}_{version}.txt"
+    s2_indices_filename = f"s2-indices_{year}_mgrs-tile-{mgrs_tile}_{version}.txt"
     if os.path.exists(
         os.path.join(
-            "data", "gee_pipeline", "outputs", "s2_indices", s2_indices_filename
+            "data",
+            "gee_pipeline",
+            "outputs",
+            "s2_indices_per_mgrs_tile",
+            s2_indices_filename,
         )
     ):
         logger.debug(f"Loading s2_indices_filtered from file: {s2_indices_filename}")
         with open(
             os.path.join(
-                "data", "gee_pipeline", "outputs", "s2_indices", s2_indices_filename
+                "data",
+                "gee_pipeline",
+                "outputs",
+                "s2_indices_per_mgrs_tile",
+                s2_indices_filename,
             ),
             "r",
         ) as f:
             s2_indices_filtered = f.read().splitlines()
     else:
-        # load all preprocessed lists of mgrs tiles
-        if isinstance(eco_id, int):
-            df = pd.read_csv(
-                os.path.join(
-                    "data",
-                    "gee_pipeline",
-                    "outputs",
-                    "mgrs_tiles",
-                    f"mgrs_tiles_ecoregion_{eco_id}.csv",
-                )
-            )
-            mgrs_tiles = df["mgrs_tile"].tolist()
-        elif isinstance(eco_id, list):
-            mgrs_tiles = []
-            for eco in eco_id:
-                df = pd.read_csv(
-                    os.path.join(
-                        "data",
-                        "gee_pipeline",
-                        "outputs",
-                        "mgrs_tiles",
-                        f"mgrs_tiles_ecoregion_{eco}.csv",
-                    )
-                )
-                mgrs_tiles.extend(df["mgrs_tile"].tolist())
-            mgrs_tiles = list(set(mgrs_tiles))
-
         s2_indices_filtered = get_s2_indices_filtered(
-            ecoregion_boundary=geometry.bounds(),
-            start_date=start_date,
-            end_date=end_date,
-            mgrs_tiles=mgrs_tiles,
+            mgrs_tiles=current_mgrs_tiles, start_date=start_date, end_date=end_date
         )
         logger.debug(f"Saving s2_indices_filtered to file: {s2_indices_filename}")
         # save s2_indices_filtered for later use
         with open(
             os.path.join(
-                "data", "gee_pipeline", "outputs", "s2_indices", s2_indices_filename
+                "data",
+                "gee_pipeline",
+                "outputs",
+                "s2_indices_per_mgrs_tile",
+                s2_indices_filename,
             ),
             "w",
         ) as f:
@@ -139,187 +104,131 @@ def export_ecoregion_per_mgrs_tile(
 
     if len(s2_indices_filtered) == 0:
         logger.error(
-            f"Sentinel-2 collection empty after filter for ecoregion: {eco_id}"
+            f"Sentinel-2 collection empty after filter for mgrs_tile: {mgrs_tile}"
         )
         return
 
-    mgrs_tiles = set(
-        system_index.split("_")[2][0:4] for system_index in s2_indices_filtered
+    imgc = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED").filter(
+        ee.Filter.inList("system:index", s2_indices_filtered)
     )
 
-    for mgrs_tile in mgrs_tiles:
-        current_sentinel2_indices = [
-            system_index
-            for system_index in s2_indices_filtered
-            if system_index.split("_")[2][0:4] == mgrs_tile
-        ]
-        imgc = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED").filter(
-            ee.Filter.inList("system:index", current_sentinel2_indices)
-        )
+    # determine intersecting output geometry
+    output_geometry_bbox = imgc.geometry().bounds()
 
-        # determine intersecting output geometry
-        output_geometry_bbox = imgc.geometry().bounds()
+    # apply cloud mask
+    imgc = apply_cloudScorePlus_mask(imgc)
 
-        # apply cloud mask
-        imgc = apply_cloudScorePlus_mask(imgc)
+    # Apply the function to each image in the collection
+    # imgc = imgc.map(add_random_property)
+    imgc = ee.ImageCollection(add_random_ensemble_assignment(imgc))
 
-        # Apply the function to each image in the collection
-        # imgc = imgc.map(add_random_property)
-        imgc = ee.ImageCollection(add_random_ensemble_assignment(imgc))
+    # add angles to bands
+    imgc = imgc.map(add_angles_from_metadata_to_bands)
 
-        # add angles to bands
-        imgc = imgc.map(add_angles_from_metadata_to_bands)
+    imgc_preds = {}
 
-        imgc_preds = {}
-
-        for trait in CONFIG_GEE_PIPELINE["PIPELINE_PARAMS"]["TRAITS"]:
-            gee_preds = {}
-            models = load_model_ensemble(trait=trait, models=["mlp"])
-            for i, (model_name, model) in enumerate(models.items()):
-                imgc_i = imgc.filter(ee.Filter.eq("random_ensemble_assignment", i + 1))
-                gee_preds[model_name] = eePipelinePredictMap(
-                    pipeline=model["pipeline"],
-                    imgc=imgc_i,
-                    trait=trait,
-                    model_config=model["config"],
-                    min_max_bands=model["min_max_bands"],
-                    min_max_label=model["min_max_label"],
-                )
-
-            imgc_preds[trait] = reduce(lambda x, y: x.merge(y), gee_preds.values())
-
-        # link the collections if more than one trait
-        if len(CONFIG_GEE_PIPELINE["PIPELINE_PARAMS"]["TRAITS"]) > 1:
-            imgc_preds_combined = reduce(
-                lambda x, y: x.combine(y), list(imgc_preds.values())
+    for trait in CONFIG_GEE_PIPELINE["PIPELINE_PARAMS"]["TRAITS"]:
+        gee_preds = {}
+        models = load_model_ensemble(trait=trait, models=["mlp"])
+        for i, (model_name, model) in enumerate(models.items()):
+            imgc_i = imgc.filter(ee.Filter.eq("random_ensemble_assignment", i + 1))
+            gee_preds[model_name] = eePipelinePredictMap(
+                pipeline=model["pipeline"],
+                imgc=imgc_i,
+                trait=trait,
+                model_config=model["config"],
+                min_max_bands=model["min_max_bands"],
+                min_max_label=model["min_max_label"],
             )
-        else:
-            imgc_preds_combined = imgc_preds[trait]
 
-        # explicitly cast toFloat
-        imgc_preds_combined = imgc_preds_combined.map(lambda img: img.toFloat())
+        imgc_preds[trait] = reduce(lambda x, y: x.merge(y), gee_preds.values())
 
-        # collapse to mean and stddev
-        output_image = collapse_to_mean_and_stddev_multi_trait(imgc_preds_combined)
-
-        # mask permament water bodies :80: permanent water bodies at 10 meter resolution
-        water_mask_2020 = ee.ImageCollection("ESA/WorldCover/v100").first()
-        output_image = output_image.updateMask(water_mask_2020.neq(80))
-
-        # Set export parameters
-        year_start_string = str(year) + "0101"
-        year_end_string = str(year) + "1231"
-        epsg_code = get_epsg_code_from_mgrs(mgrs_tile)
-        epsg_code_gee = f"EPSG:{epsg_code}"
-        epsg_string = f"epsg-{epsg_code}"
-        if len(CONFIG_GEE_PIPELINE["PIPELINE_PARAMS"]["TRAITS"]) == 1:
-            traits_string = CONFIG_GEE_PIPELINE["PIPELINE_PARAMS"]["TRAITS"][0]
-        else:
-            traits_string = "-".join(CONFIG_GEE_PIPELINE["PIPELINE_PARAMS"]["TRAITS"])
-        if isinstance(eco_id, int):
-            system_index = f"{traits_string}_rtm-mlp_mean-std-n_{output_resolution}m_s_{year_start_string}_{year_end_string}_eco-{eco_id}-{mgrs_tile}_{epsg_string}_{version}"
-        elif isinstance(eco_id, list):
-            eco_ids_string = "-".join(map(str, eco_id))
-            system_index = f"{traits_string}_rtm-mlp_mean-std-n_{output_resolution}m_s_{year_start_string}_{year_end_string}_eco-{eco_ids_string}-{mgrs_tile}_{epsg_string}_{version}"
-
-        output_image = (
-            output_image.set(
-                "system:time_start", ee.Date.fromYMD(int(year), 1, 1).millis()
-            )
-            .set("system:time_end", ee.Date.fromYMD(int(year), 12, 31).millis())
-            .set("year", year)
-            .set("version", version)
-            .set("ecoregion_id", eco_id)
-            .set("system:index", system_index)
-            .set("mgrs_tile", mgrs_tile)
+    # link the collections if more than one trait
+    if len(CONFIG_GEE_PIPELINE["PIPELINE_PARAMS"]["TRAITS"]) > 1:
+        imgc_preds_combined = reduce(
+            lambda x, y: x.combine(y), list(imgc_preds.values())
         )
+    else:
+        imgc_preds_combined = imgc_preds[trait]
 
-        # Export the image
-        imgc_folder = (
-            CONFIG_GEE_PIPELINE["GEE_FOLDERS"]["ASSET_FOLDER"]
-            + f"/{traits_string}_predictions-mlp_{output_resolution}m_{CONFIG_GEE_PIPELINE['PIPELINE_PARAMS']['VERSION']}/"
-        )
+    # explicitly cast toFloat
+    imgc_preds_combined = imgc_preds_combined.map(lambda img: img.toFloat())
 
-        task = ee.batch.Export.image.toAsset(
-            image=output_image.updateMask(export_mask),
-            description=system_index,
-            crs=epsg_code_gee,
-            assetId=imgc_folder + system_index,
-            region=output_geometry_bbox,
-            scale=output_resolution,
-            maxPixels=1e11,
-        )
-        task.start()
-        time.sleep(0.1)
+    # collapse to mean and stddev
+    output_image = collapse_to_mean_and_stddev_multi_trait(imgc_preds_combined)
+
+    # mask permament water bodies :80: permanent water bodies at 10 meter resolution
+    water_mask_2020 = ee.ImageCollection("ESA/WorldCover/v100").first()
+    output_image = output_image.updateMask(water_mask_2020.neq(80))
+
+    # Set export parameters
+    year_start_string = str(year) + "0101"
+    year_end_string = str(year) + "1231"
+    epsg_code = get_epsg_code_from_mgrs(mgrs_tile)
+    epsg_code_gee = f"EPSG:{epsg_code}"
+    epsg_string = f"epsg-{epsg_code}"
+    if len(CONFIG_GEE_PIPELINE["PIPELINE_PARAMS"]["TRAITS"]) == 1:
+        traits_string = CONFIG_GEE_PIPELINE["PIPELINE_PARAMS"]["TRAITS"][0]
+    else:
+        traits_string = "-".join(CONFIG_GEE_PIPELINE["PIPELINE_PARAMS"]["TRAITS"])
+
+    system_index = f"{traits_string}_rtm-mlp_mean-std-n_{output_resolution}m_s_{year_start_string}_{year_end_string}_T{mgrs_tile}_{epsg_string}_{version}"
+
+    output_image = (
+        output_image.set("system:time_start", ee.Date.fromYMD(int(year), 1, 1).millis())
+        .set("system:time_end", ee.Date.fromYMD(int(year), 12, 31).millis())
+        .set("year", year)
+        .set("version", version)
+        .set("system:index", system_index)
+        .set("mgrs_tile", mgrs_tile)
+    )
+
+    # Export the image
+    imgc_folder = (
+        CONFIG_GEE_PIPELINE["GEE_FOLDERS"]["ASSET_FOLDER"]
+        + f"/{traits_string}_predictions-mlp_{output_resolution}m_{CONFIG_GEE_PIPELINE['PIPELINE_PARAMS']['VERSION']}/"
+    )
+
+    task = ee.batch.Export.image.toAsset(
+        image=output_image,
+        description=system_index,
+        crs=epsg_code_gee,
+        assetId=imgc_folder + system_index,
+        region=output_geometry_bbox,
+        scale=output_resolution,
+        maxPixels=1e11,
+    )
+    task.start()
+    time.sleep(0.1)
 
 
-def global_export_concurrent():
-    # export all ecoregions
-    ecoregions = ee.FeatureCollection("RESOLVE/ECOREGIONS/2017")
-    all_ecoregions = ecoregions.aggregate_array("ECO_ID").getInfo()
-
-    ecoregions_to_exclude = pd.read_csv(
+def global_export_mgrs_tiles():
+    mgrs_tiles = pd.read_csv(
         os.path.join(
-            "data", "phenology_pipeline", "outputs", "ecoregions_to_exclude_all.csv"
+            "data",
+            "gee_pipeline",
+            "outputs",
+            "mgrs_tiles",
+            "mgrs_tiles_all_land_ecoregions.csv",
         )
     )
-    ecoregions_simplifier = get_config("ecoregions_simple")
-    ecoregions_in_simplifier = set(
-        [item for row in ecoregions_simplifier["same_pheno"] for item in row]
-        + [item for row in ecoregions_simplifier["close_pheno"] for item in row]
-    )
+    mgrs_tiles_list = list(set(mgrs_tiles["mgrs_tile_3"].tolist()))
 
-    ecoregions_process_single_list = list(
-        (
-            set(all_ecoregions)
-            - set(ecoregions_to_exclude["ECO_ID"])
-            - set(ecoregions_in_simplifier)
-        )
-    )
-
-    ecoregions_process_multi_list = [
-        *ecoregions_simplifier["same_pheno"],
-        *ecoregions_simplifier["close_pheno"],
-    ]
-
-    # DEBUG
-    if True:
-        ecoregions_process_multi_list = []
-        ecoregions_process_single_list = [
-            450,
-            451,
-            617,
-            502,
-            527,
-            533,
-            553,
-            14,
-            44,
-            113,
-            62,
-            8,
-            43,
-            51,
-            1,
-            39,
-        ]
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=24) as executor:
         futures = [
-            executor.submit(export_ecoregion_per_mgrs_tile, eco_id)
-            for eco_id in [
-                *ecoregions_process_single_list,
-                *ecoregions_process_multi_list,
-            ]
+            executor.submit(export_mgrs_tile, mgrs_tile)
+            for mgrs_tile in mgrs_tiles_list
         ]
-        for future in concurrent.futures.as_completed(futures):
+        for future in tqdm(
+            concurrent.futures.as_completed(futures), total=len(futures)
+        ):
             try:
                 future.result()  # If the task raised an exception, this will raise it here
             except Exception as e:
-                logger.error(f"Error exporting ecoregion: {e}")
+                logger.error(f"Error exporting mgrs_tile: {e}")
 
-    logger.info("All ecoregions export tasks started")
+    logger.info("All mgrs_tile export tasks started")
 
 
 if __name__ == "__main__":
-    global_export_concurrent()
+    global_export_mgrs_tiles()
--- gee_pipeline/utilsTiles.py
+++ gee_pipeline/utilsTiles.py
@@ -17,18 +17,53 @@ from gee_pipeline.utilsPhenology import add_linear_weight
 CONFIG_GEE_PIPELINE = get_config("gee_pipeline")
 
 
+def return_mgrs_bounding_box(mgrs_tiles: list) -> ee.Geometry.BBox:
+    zone_number = int(mgrs_tiles[0][0:2])
+    zone_letter = mgrs_tiles[0][2]
+
+    longitudes = {
+        "C": [-80, -72],
+        "D": [-72, -64],
+        "E": [-64, -56],
+        "F": [-56, -48],
+        "G": [-48, -40],
+        "H": [-40, -32],
+        "J": [-32, -24],
+        "K": [-24, -16],
+        "L": [-16, -8],
+        "M": [-8, 0],
+        "N": [0, 8],
+        "P": [8, 16],
+        "Q": [16, 24],
+        "R": [24, 32],
+        "S": [32, 40],
+        "T": [40, 48],
+        "U": [48, 56],
+        "V": [56, 64],
+        "W": [64, 72],
+        "X": [72, 84],
+    }
+
+    west = -180 + (zone_number - 1) * 6
+    south = longitudes[zone_letter][0]
+    east = west + 6
+    north = longitudes[zone_letter][1]
+
+    return ee.Geometry.BBox(west, south, east, north)
+
+
 def get_s2_indices_filtered(
-    ecoregion_boundary: ee.Geometry,
     start_date: ee.Date,
     end_date: ee.Date,
-    mgrs_tiles: list = None,
+    mgrs_tiles: list,
 ) -> pd.DataFrame:
     # load s2 data
     bands = ["B2", "B3", "B4", "B5", "B6", "B7", "B8", "B8A", "B11", "B12"]
+    bounding_box = return_mgrs_bounding_box(mgrs_tiles)
     imgc = (
         ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
-        .filterBounds(ecoregion_boundary)
         .filterDate(start_date, end_date)
+        .filterBounds(bounding_box.buffer(1e6))
         .filter(ee.Filter.inList("MGRS_TILE", mgrs_tiles))
         .filter(
             ee.Filter.lt(
@@ -43,7 +78,7 @@ def get_s2_indices_filtered(
                 ee.Filter.gt("system:asset_size", 1000000),
             )
         )
-        .select(bands)
+        .select([*bands, "SCL"])
     )
 
     # filter imgc by grouping by mgrs tile and orbit number
@@ -72,14 +107,15 @@ def get_epsg_code_from_mgrs(mgrs_zone_number: str):
     Returns:
     - int: The EPSG code.
     """
-    assert len(mgrs_zone_number) == 4, "MGRS zone number must be a four digit string."
+    if len(mgrs_zone_number) == 4 and mgrs_zone_number[0] == "T":
+        mgrs_zone_number = mgrs_zone_number[1:]
     if mgrs_zone_number[-1] in string.ascii_uppercase[13:26]:
         southern_hemisphere = False
     elif mgrs_zone_number[-1] in string.ascii_uppercase[0:13]:
         southern_hemisphere = True
     else:
         raise ValueError("Invalid MGRS zone number.")
-    zone_number = int(mgrs_zone_number[1:3])
+    zone_number = int(mgrs_zone_number[0:2])
     crs = CRS.from_dict(
         {
             "proj": "utm",
@@ -126,9 +162,13 @@ def add_ndvi_weight(image: ee.Image) -> ee.Image:
 
 
 def add_evi_weight(image: ee.Image) -> ee.Image:
+    # use SCL to mask out all snow and water pixels, also defective pixels: mask out 0, 1, 2, 6, 11
+    scl = image.select("SCL")
+    scl_mask = scl.remap([0, 1, 2, 6, 11], [0, 0, 0, 0, 0], 1)
 
+    # then mask based on esa world cover
     worldcover = ee.ImageCollection("ESA/WorldCover/v100").first()
-    natural_classes = [10, 20, 30, 60, 70, 80, 90, 95, 100]
+    natural_classes = [10, 20, 30, 60, 70, 90, 95, 100]
     natural_mask = worldcover.remap(
         natural_classes, ee.List.repeat(1, len(natural_classes)), 0
     )
@@ -141,7 +181,8 @@ def add_evi_weight(image: ee.Image) -> ee.Image:
             "BLUE": image.select("B2").divide(10000),
         },
     ).rename("evi")
-    evi = evi.updateMask(natural_mask)
+    # mask out pixels that should not contribute to evi calculation
+    evi = evi.updateMask(natural_mask).updateMask(scl_mask)
 
     mean_evi = evi.reduceRegion(
         reducer=ee.Reducer.mean(), geometry=image.geometry(), scale=500
@@ -152,6 +193,12 @@ def add_evi_weight(image: ee.Image) -> ee.Image:
     evi_weight = ee.Number(1).subtract(mean_evi).multiply(2)
     cloud_pheno_weight_combined = cloudy_pixel_percentage.add(evi_weight)
 
+    system_asset_size = image.getNumber("system:asset_size")
+
+    water_percentage = image.getNumber("WATER_PERCENTAGE").divide(100)
+    snow_ice_percentage = image.getNumber("SNOW_ICE_PERCENTAGE").divide(100)
+    vegetation_percentage = image.getNumber("VEGETATION_PERCENTAGE").divide(100)
+
     return image.set(
         "cloud_pheno_image_weight",
         cloud_pheno_weight_combined,
@@ -161,6 +208,14 @@ def add_evi_weight(image: ee.Image) -> ee.Image:
         evi_weight,
         "cloudy_pixel_percentage",
         cloudy_pixel_percentage,
+        "system_asset_size",
+        system_asset_size,
+        "water_percentage",
+        water_percentage,
+        "snow_ice_percentage",
+        snow_ice_percentage,
+        "vegetation_percentage",
+        vegetation_percentage,
     )
 
 
@@ -191,6 +246,10 @@ def groupby_mgrs_orbit_pandas(
                 "ndvi_weight": img.getNumber("ndvi_weight"),
                 "mean_evi": img.getNumber("mean_evi"),
                 "evi_weight": img.getNumber("evi_weight"),
+                "system_asset_size": img.getNumber("system_asset_size"),
+                "water_percentage": img.getNumber("water_percentage"),
+                "snow_ice_percentage": img.getNumber("snow_ice_percentage"),
+                "vegetation_percentage": img.getNumber("vegetation_percentage"),
             },
         )
 
@@ -234,18 +293,45 @@ def groupby_mgrs_orbit_pandas(
         by=["group", "cloud_pheno_image_weight"], ascending=[True, True]
     )
 
-    # per group, drop images if their mean_evi is below max(mean_evi) - MAX_EVI_DIFFERENCE
+    # filter out all evi larger than 1.0 and smaller than -1.0
+    df_sorted = df_sorted[
+        (df_sorted["mean_evi"] <= 1.0) & (df_sorted["mean_evi"] > -1.0)
+    ]
+
+    # filter out all images where snow_ice_percetange - 4*vegetation_percentage is greater than 0.0
+    df_sorted = df_sorted[
+        (df_sorted["snow_ice_percentage"] - 4 * df_sorted["vegetation_percentage"])
+        <= 0.0
+    ]
+
+    # group by group and date of acquisition, only keep largest image (by system_asset_size)
+    df_sorted["acquisition_date"] = df_sorted["s2_index"].str.slice(0, 8)
+    df_sorted = (
+        df_sorted.groupby(["group", "acquisition_date"])
+        .apply(lambda x: x.nlargest(1, "system_asset_size"))
+        .reset_index(drop=True)
+    )
+
+    # per group, drop images if their mean_evi is below 0.9 quantile - MAX_EVI_DIFFERENCE
     df_sorted["mean_evi_diff"] = df_sorted.groupby("group")["mean_evi"].transform(
-        lambda x: x.max() - x
+        lambda x: x.quantile(CONFIG_GEE_PIPELINE["S2_FILTERING"]["EVI_MAX_PERCENTILE"])
+        - x
     )
     df_sorted = df_sorted[
         df_sorted["mean_evi_diff"]
-        <= CONFIG_GEE_PIPELINE["PIPELINE_PARAMS"]["MAX_EVI_DIFFERENCE"]
+        <= CONFIG_GEE_PIPELINE["S2_FILTERING"]["MAX_EVI_DIFFERENCE"]
     ]
 
     # from the remaining images, select the top MAX_IMAGES_PER_GROUP images per group
-    df_grouped = df_sorted.groupby("group").head(
-        CONFIG_GEE_PIPELINE["PIPELINE_PARAMS"]["MAX_IMAGES_PER_GROUP"]
+    df_grouped = (
+        df_sorted.groupby("group")
+        .apply(
+            lambda x: x.nsmallest(
+                CONFIG_GEE_PIPELINE["S2_FILTERING"]["MAX_IMAGES_PER_GROUP"],
+                "cloud_pheno_image_weight",
+            )
+        )
+        .reset_index(drop=True)
     )
 
     logger.debug(f"imgc size after grouping and filtering: {df_grouped.shape[0]}")
@@ -291,9 +377,42 @@ def save_mgrs_tiles_all_ecoregions():
         executor.map(save_mgrs_tiles_for_ecoregion, eco_ids)
 
 
+def get_all_land_mgrs_tiles():
+    # load all csv file in data/gee_pipeline/outputs/mgrs_tiles, add eco_id as column
+    foldername = os.path.join("data", "gee_pipeline", "outputs", "mgrs_tiles_per_eco")
+    files = [f for f in os.listdir(foldername) if f.endswith(".csv")]
+    df = pd.DataFrame()
+    for file in files:
+        eco_id = file.split("_")[-1].split(".")[0]
+        df_temp = pd.read_csv(os.path.join(foldername, file))
+        df_temp["eco_id"] = eco_id
+        df = pd.concat([df, df_temp])
+
+    # now read file data/phenology_pipeline/outputs/ecoregions_to_exclude_all.csv
+    df_exclude = pd.read_csv(
+        os.path.join(
+            "data", "phenology_pipeline", "outputs", "ecoregions_to_exclude_all.csv"
+        )
+    )
+
+    # filter out the ecoregions to exclude
+    df = df[~df["eco_id"].isin(df_exclude["ECO_ID"])]
+
+    # from 5 letter mgrs tile, get the 3 letter mgrs tile
+    df["mgrs_tile_3"] = df["mgrs_tile"].str[:3]
+
+    # save file
+    df.to_csv(
+        os.path.join(foldername, "mgrs_tiles_all_land_ecoregions.csv"), index=False
+    )
+
+    return df
+
+
 if __name__ == "__main__":
 
     ee.Initialize(project="ee-speckerfelix")
 
-    save_mgrs_tiles_all_ecoregions()
+    get_all_land_mgrs_tiles()
+    # save_mgrs_tiles_all_ecoregions()
     # get_s2_indices_filtered(
